284348b99eb74f85de82e935ad15b6ae5033644c
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-04-08 11:05:17 +0000

    Call register function from auth controller directory.

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const { register } = require('../controllers/authController');
 
 // @route POST    /api/v1/auth/register
 // @description  Register a new user
 // @access      Public
 
-router.post('/register', (req, res) => {
-  res.send('Register endpoint hit');
-})
+router.post('/register', register);
 
 module.exports = router;

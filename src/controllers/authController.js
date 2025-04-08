6debc4f1af0582922c9f5113d24eb68d134505b3
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-04-08 13:17:48 +0000

    Create login logic

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -16,6 +16,7 @@ exports.register = async (req, res) => {
     }
     // password hashing
     const salt = await bcrypt.genSalt(10);
+    console.log(salt);
     const hashedPassword = await bcrypt.hash(password, salt);
     const user = new User({
       first_name,
@@ -32,5 +33,36 @@ exports.register = async (req, res) => {
   } catch (error) {
     console.error("Error registering user: ", error);
     res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+
+exports.login = async (req, res) => {
+  try{
+    const { email, password } = req.body;
+    // validation
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required"});
+    }
+    // check if user exists
+    const existingUser = await User.findOne({ email });
+    if(!existingUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    // password check 
+    const isPasswordValid = await bcrypt.compare(password, existingUser.password);
+    console.log(isPasswordValid);
+    if (!isPasswordValid) {
+      return res.status(404).json({ message: "Invalid credentials" }); 
+    }
+    // token generation
+    const accessToken = await jwt.sign({ id: existingUser._id }, process.env.JWT_SECRET, {
+      expiresIn: '15mins'
+    });
+     // login success
+     return res.status(200).json({ message: "Login successful", accessToken, user: existingUser});
+  } catch (error) {
+    console.error("Error logging in user: ", error);
+    res.status(500).json({ message: "Internal server error"});
   }
 }

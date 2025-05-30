138d88d74f481e3a04eaccda8aaf8d14dc01dd88
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-05-30 06:50:41 +0000

    Complete user crud operations

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -37,3 +37,18 @@ exports.updateUser = async(req, res) => {
     res.status(500).json({ status: false, message: 'Internal Server Error' });
   }
 }
+
+exports.deleteUser = async(req, res) => {
+  try {
+    const { userId } = req.user;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ status: false, message: 'No user found' }); 
+    }
+    await User.findByIdAndDelete(userId);
+    res.status(200).json({ status: true, message: 'User successfully deleted' });
+  } catch (error) {
+    console.error('Error deleting user:', error);
+    res.status(500).json({ status: false, message: 'Internal Server Error' });
+  }
+}

473de72830d871e70062c759a374089553162856
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-05-30 06:42:28 +0000

    Begin user crud operations

---
+++ src/controllers/userController.js
@@ -0,0 +1,39 @@
+const User = require("../models/User");
+
+exports.getUser = async(req, res) => {
+  try {
+    const { userId } = req.user
+    const user = await User.findById(userId);
+    if(!user) {
+      return res.status(404).json({ status: false, message: 'No user found' });
+    }
+    res.status(200).json({ status: true, message: 'user found', user });
+  } catch (error) {
+    console.error('Error getting user:', error);
+    res.status(500).json({ status: false, message: 'Internal Server Error' });
+  }
+}
+
+exports.updateUser = async(req, res) => {
+  try {
+    const { userId } = req.user;
+    const { first_name, last_name, email } = req.body;
+    // Validate input
+    if (!first_name || !last_name || !email) {
+      return res.status(400).json({ status: false, message: 'All fields are required' });
+    }
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ status: false, message: 'User not found' });
+    }
+    const updateUser = await User.findByIdAndUpdate(userId, {
+      first_name,
+      last_name,
+      email
+    }, { new: true });
+    res.status(200).json({ status: true, message: 'User updated successfully', user: updateUser });
+  } catch (error) {
+    console.error('Error updating user:', error);
+    res.status(500).json({ status: false, message: 'Internal Server Error' });
+  }
+}

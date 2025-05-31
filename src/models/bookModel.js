09662b4fabe7482e9ad731c57a6b89346ec7b4d9
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-05-31 19:16:11 +0000

    Refactor book model schema to enhance structure and ensure review data integrity

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const { ObjectId } = Schema.Types;
 
-const bookSchema = new mongoose.Schema({
+const bookSchema = new Schema({
   title: {
     type: String,
     required: true
@@ -25,7 +27,30 @@ const bookSchema = new mongoose.Schema({
   inStock: {
     type: Boolean,
     default: true
-  }
+  },
+  stockQuantity: {
+    type: Number,
+    required: true,
+    min: 0,
+    default: 1
+  },
+  reviews: [{
+    userId: {
+      type: ObjectId,
+      ref: 'User',
+      required: true
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5
+    },
+    comment: {
+      type: String,
+      required: true
+    }
+  }],
 }, {timestamps: true});
 
 module.exports = mongoose.model('Book', bookSchema);
